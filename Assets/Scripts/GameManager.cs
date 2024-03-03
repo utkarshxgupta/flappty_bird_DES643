633264aabc060ac9b4d2ee90f636f75ae7139240
Utkarsh <gutkarsh20@iitk.ac.in>
2024-03-04 04:21:04 +0530

    project uploaded from local

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Player player;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject gameOver;
+    [SerializeField] private GameObject bronzeMedal;
+    [SerializeField] private GameObject silverMedal;
+    [SerializeField] private GameObject goldMedal;
+
+    private int score;
+    public int Score => score;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            Application.targetFrameRate = 60;
+            DontDestroyOnLoad(gameObject);
+            Pause();
+        }
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        bronzeMedal.SetActive(false);
+        silverMedal.SetActive(false);
+        goldMedal.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+        if (score > 0 && score <= 5)
+        {
+            bronzeMedal.SetActive(true);
+        }
+        else if (score > 5 && score <= 10)
+        {
+            silverMedal.SetActive(true);
+        }
+        else if (score > 10)
+        {
+            goldMedal.SetActive(true);
+        }
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
